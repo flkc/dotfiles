fa32736d3833a5ad0c96af0586b978fbb594b266
Njord Deniaud <lambdaconda@gmail.com>
2022-02-25 22:31:48 +0100

    Small changes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #/bin/bash
 
-echo 'Set-up system packages'
+echo $'Set-up system packages\n'
 
-sudo pacman -S fzf ripgrep neovim egrep awk # TODO: use different package managers i.e. ubuntu-arch
+sudo pacman -S fzf ripgrep neovim awk # TODO: use different package managers i.e. ubuntu-arch
 
-echo 'Set-up VimPlug for neovim'
+echo $'Set-up VimPlug for neovim\n'
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo 'Initialize symlinks'
+echo $'Initialize symlinks of all files but .git, and setup.sh should to HOMEDIR\n'
 
-# TODO: refactor to accept all but .git directory
 find ./ -maxdepth 1 -printf "%f\n" | egrep -v './|.git|setup.sh' |  awk '{print "\"cp -rs $PWD/"$1" $HOME/"$1"\""}' | xargs -i sh -c "{}"
